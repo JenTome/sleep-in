7a1ce21603b6993a265ab9c6bd5d4f676d43b0b3
JenTome <tome.jen@gmail.com>
2020-01-04 11:02:34 -0500

    sleep in

---
+++ src/SleepIn.java
@@ -0,0 +1,22 @@
+
+public class SleepIn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+
+var sleptIn = sleepIn (true, true);
+System.out.println(sleptIn);
+}
+	
+public static boolean sleepIn(boolean weekday, boolean vacation) {
+	  if (weekday != true || vacation == true) {
+	  return true;
+	  }
+	  return false;
+
+}
+
+}
